d6e424629ce7639f5720679b1d9d70655ee29332
Valeska Valentin Ekklesia <106671548+ValeskaLim@users.noreply.github.com>
2025-09-13 12:45:39 +0700

    Time: 494 ms (92.63%), Space: 0B (100%) - LeetHub

---
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+select round(sum(tiv_2016), 2) as tiv_2016 
+from insurance
+where (lat, lon) in 
+    (select lat, lon
+    from insurance
+    group by lat, lon
+    having count(*) < 2)
+and tiv_2015 in (select tiv_2015
+                from insurance
+                group by tiv_2015
+                having count(*) > 1);
